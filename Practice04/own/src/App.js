addcada351f34510a294dbbc76c1489c7b500612
peterhuangfu <nightlight870213@gmail.com>
2019-03-30 14:46:21 +0800

    prac04

--- Practice04/own/src/App.js
+++ Practice04/own/src/App.js
@@ -5,7 +5,7 @@ import Footer from './components/footer';
 class Homepage extends Component {
   constructor(props) {
     super(props);
-    this.state = { command: 'all', left: 0 };
+    this.state = { clear: false, command: 'all', left: 0 };
   }
 
   update_num = (num) => {
@@ -15,20 +15,21 @@ class Homepage extends Component {
   filterAll = () => { this.setState(() => ({ command: 'all'})); }
   filterAct = () => { this.setState(() => ({ command: 'active'})); };
   filterCom = () => { this.setState(() => ({ command: 'completed'})); };
-  clear = () => { 
-
-  };
+  clear_start = () => { this.setState(() => ({ clear: true })); setTimeout(() => this.clear_init, 1000); };
+  clear_init = () => { this.setState(() => ({ clear: false })); };
 
   render() {
     return (
       <div className="todo-app__root">
         <header className="todo-app__header"><h1 className="todo-app__title">TODO</h1></header>
         <Section command={this.state.command}
+                 ifClear={this.state.clear}
+                 clear={this.clear_init}
                  update_num={this.update_num}/>
         <Footer filterAll={this.filterAll}
                 filterAct={this.filterAct}
                 filterCom={this.filterCom}
-                clear={this.clear}
+                clear={this.clear_start}
                 left={this.state.left}/>
       </div>
     );
--- Practice04/own/src/components/section.js
+++ Practice04/own/src/components/section.js
@@ -28,7 +28,7 @@ class Section extends Component {
       const newItem = { node: <Item id={this.state.id} check={this.check} delete={this.del} content={content}/>, isComplete: false };
       this.arg.push(this.state.id);
       this.setState(state => ({ id: state.id + 1 }));
-      this.setState(state => ({ list: state.list.concat([newItem])}));
+      this.setState(state => ({ list: state.list.concat([newItem]) }));
       this.props.update_num(this.state.list.filter(e => !e.isComplete).length);
       e.target.value = '';
     }
@@ -36,12 +36,11 @@ class Section extends Component {
 
   render() {
     const situation = this.props.command;
-    console.log(situation);
     return (
       <section className="todo-app__main">
         <input className="todo-app__input" placeholder="What needs to be done ?" onKeyPress={this.handler}/>
         <ul className="todo-app__list" id="todo-list">
-          { this.state.list.map(function(item, i) {
+          { this.state.list.map(function(item, i) {            
             if(situation === 'active') {
               if(item.isComplete === false) {
                 return <div key={i}>{item.node}</div>;
@@ -55,7 +54,6 @@ class Section extends Component {
               else return <div key={i}></div>
             }
             else if(situation === 'all') {
-              console.log(item);
               return <div key={i}>{item.node}</div>;
             }
             return <div key={i}></div>;
