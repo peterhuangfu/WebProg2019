d5060eb0bd02746fbb75fd976e81c9f213b7de08
peterhuangfu <nightlight870213@gmail.com>
2019-04-06 16:59:38 +0800

    practice05

--- Practice05/own/src/containers/CalcApp.js
+++ Practice05/own/src/containers/CalcApp.js
@@ -9,12 +9,16 @@ class CalcApp extends React.Component {
     this.state = { num: '0', cal: '', extra: '' };
     this.notFirst = false;
     this.previous = '0';
+    this.ago = '0';
   }
 
   resetState = () => {
     this.setState(() => ({ num: '0' }));
     this.setState(() => ({ cal: '' }));
+    this.setState(() => ({ extra: '' }));
     this.notFirst = false;
+    this.previous = '0';
+    this.ago = '0';
   }
 
   calcu = (num) => {
@@ -27,49 +31,79 @@ class CalcApp extends React.Component {
             this.setState(() => ({ num: ans.toString() }));
             this.setState(() => ({ cal: ans.toString() }));
           }
-          else this.setState(state => ({ cal: state.cal.substr(0, state.cal.length) }));
+          else 
+            this.setState(state => ({ cal: state.cal.substr(0, state.cal.length) }));
         }
         else this.setState(state => ({ cal: state.cal.substr(0, state.cal.length-1) }));
 
         if(num !== '=') this.setState(state => ({ cal: state.cal.concat(num) }));
-        // setTimeout(() => console.log(this.state.cal), 50);
+        setTimeout(() => console.log(this.state.cal), 50);
+        if(isNaN(parseInt(this.previous)) !== isNaN(parseInt(num)) || this.ago !== '=') this.ago = this.previous;
         this.previous = num;
       }
       else { // 運算
         let num1 = ''; let num2 = ''; let log = ''; let ans;
-        for(let i = 0; i < this.state.cal.length; i++) {
-          if(isNaN(parseFloat(this.state.cal[i])) && this.state.cal[i] !== '.') {
-            log = this.state.cal[i];
-            num1 = this.state.cal.substr(0,i);
-            num2 = this.state.cal.substr(i+1, this.state.cal.length-1-i);
-            break;
+        if(num === '=' && this.ago === '=') {
+          if(this.state.cal[0] === '-') {
+            num1 = parseFloat(this.state.cal.substr(1, this.state.cal.length-1));
+            num1 *= (-1);
+          }
+          else num1 = parseFloat(this.state.cal);
+          ans = this.count(num1, parseFloat(this.state.extra.substr(1, this.state.extra.length-1)), this.state.extra[0]);
+          this.setState(() => ({ num: ans.toString() }));
+          this.setState(() => ({ cal: ans.toString() }));
+          console.log('herA');
+        }
+        else {
+          for(let i = 0; i < this.state.cal.length; i++) {
+            if(isNaN(parseFloat(this.state.cal[i])) && this.state.cal[i] !== '.') {
+              if(i === 0) continue;
+              else {
+                if(isNaN(parseFloat(this.state.cal[0]))) {
+                  num1 = this.state.cal.substr(1, i-1);
+                  num1 *= (-1);
+                }
+                else num1 = this.state.cal.substr(0, i);
+              }
+              log = this.state.cal[i];
+              num2 = this.state.cal.substr(i+1, this.state.cal.length-1-i);
+              console.log('here');
+              // console.log(num1, num2);
+              break;
+            }
+          }
+          ans = this.count(parseFloat(num1), parseFloat(num2), log)
+          
+          if(log !== '') {
+            this.setState(() => ({ cal: ans.toString() }));
+            this.setState(() => ({ num: ans.toString() }));
+            this.setState(() => ({ extra: log + num2 }));
           }
         }
-        ans = this.count(parseFloat(num1), parseFloat(num2), log)
-        
-        if(log !== '') {
-          this.setState(() => ({ cal: ans.toString() }));
-          this.setState(() => ({ num: ans.toString() }));
-          this.setState(() => ({ extra: log + num2 }));
+
+        if(num !== '=') {
+          if(this.previous !== '=') this.setState(state => ({ cal: state.cal.concat(num) }));
         }
-        if(num !== '=') this.setState(state => ({ cal: state.cal.concat(num) }));
+
+        if(isNaN(parseInt(this.previous)) !== isNaN(parseInt(num)) || this.ago !== '=') this.ago = this.previous;
         this.previous = num;
-        // setTimeout(() => console.log(this.state.cal), 50);
+        setTimeout(() => console.log(this.state.cal), 50);
       }
     }
     else { // 數字 -> 累積
-      if(this.num === '=') this.setState(() => ({ cal: '' }));
-      if(this.num !== '=') {
-        this.setState(state => ({ cal: state.cal.concat(num) }));
-        if(isNaN(parseFloat(this.previous))) this.setState(() => ({ num: '' }));
-        if(this.notFirst === false) {
-          this.setState(() => ({ num: num }));
-          this.notFirst = true;
-        }
-        else this.setState(state => ({ num: state.num.concat(num) }));
+      if(this.previous === '=') this.setState(() => ({ cal: num }));
+      else this.setState(state => ({ cal: state.cal.concat(num) }));
+
+      if(isNaN(parseFloat(this.previous))) this.setState(() => ({ num: '' }));
+      if(this.notFirst === false) {
+        this.setState(() => ({ num: num }));
+        this.notFirst = true;
       }
+      else this.setState(state => ({ num: state.num.concat(num) }));
+      
+      if(isNaN(parseInt(this.previous)) !== isNaN(parseInt(num)) || this.ago !== '=') this.ago = this.previous;
       this.previous = num;
-      // setTimeout(() => console.log(this.state.cal), 50);
+      setTimeout(() => console.log(this.state.cal), 50);
     }
   }
 
