2da2bc89dc12721f0207dc689f2a6bbf0e623b77
peterhuangfu <nightlight870213@gmail.com>
2019-04-07 15:05:35 +0800

    practice05

--- Practice05/own/src/components/__tests__/CalcButton.spec.js
+++ Practice05/own/src/components/__tests__/CalcButton.spec.js
@@ -3,6 +3,11 @@ import { mount } from 'enzyme';
 
 import CalcButton from '../CalcButton';
 
+const Enzyme = require('enzyme');
+const EnzymeAdapter = require('enzyme-adapter-react-16');
+// Setup enzyme's react adapter
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
 it('can assign extra class to button', () => {
   const element = mount(
     <CalcButton
